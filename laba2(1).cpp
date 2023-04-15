15ae4fd9c8c3e57a7b9f3bd6407e97c4ee4599c2
antonkovalevv <116675315+antonkovalevv@users.noreply.github.com>
2023-04-15 23:57:21 +0300

    Add files via upload

---
+++ laba2(1).cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <typeinfo>
+
+class Class1 {
+public:
+	virtual ~Class1() {}
+	virtual Class1* clone() const { return new Class1(*this); }
+};
+
+class Class2 : public Class1 {
+public:
+	virtual Class1* clone() const override { return new Class2(*this); }
+};
+
+void my_copy(const Class1& obj, std::vector<Class1*>& db) {
+	db.push_back(obj.clone());
+}
+
+int main() {
+	std::vector<Class1*> db;
+
+	Class1 obj1;
+	Class2 obj2;
+
+	my_copy(obj1, db);
+	my_copy(obj2, db);
+
+	for (auto& obj : db) {
+		std::cout << typeid(*obj).name() << std::endl;
+		delete obj;
+	}
+
+	return 0;
+}
